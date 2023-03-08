5c8c0412ab843aa44fa7a80a572202d17c420e6e
anastaviki <64853158+anastaviki@users.noreply.github.com>
2023-03-08 16:46:23 +0100

    Statistics DB HW done

--- Hometask 2 - DB statistics/Hometask 2building a DB statistics.sql
+++ Hometask 2 - DB statistics/Hometask 2building a DB statistics.sql
@@ -1,16 +1,12 @@
-
-
-
- 
 drop procedure if exists  Statistics_pr;
 
-Go
-CREATE PROCEDURE Statistics_pr
+Go 
+CREATE PROCEDURE Statistics_pr -- create procedure with input parameters
     @p_DatabaseName NVARCHAR(MAX),
     @p_SchemaName NVARCHAR(MAX),
     @p_TableName NVARCHAR(MAX)
 AS
-DECLARE @v_TablesList TABLE ([Table_Name] VARCHAR(100), [Column_name]VARCHAR(100));
+DECLARE @v_TablesList TABLE ([Table_Name] VARCHAR(100), [Column_name]VARCHAR(100)); --create temporary table with list of tables and colunms
 	if @p_TableName ='%'
 		begin 
 			INSERT
@@ -42,92 +38,176 @@ DECLARE @v_TablesList TABLE ([Table_Name] VARCHAR(100), [Column_name]VARCHAR(100
 
 
 
---DECLARE @v_Query NVARCHAR(MAX);
+DECLARE @v_Query NVARCHAR(MAX); -- char for query
 WITH
 	tbl_list AS
 	(
 		SELECT
 			[Table_Name],[Column_name]
-			,LEAD([Column_name]) OVER (ORDER BY [Table_Name],[Column_name]) [lead_row]
+			,LEAD([Column_name]) OVER (ORDER BY [Table_Name],[Column_name]) [lead_row] -- use lead to check if it is last record in temporary table
 		FROM @v_TablesList
-	)
+	),
+query_not_agg AS( -- generate query
 SELECT
 			CASE
 				WHEN [lead_row] IS NOT NULL 
-					THEN 'Select   '+ 
-	''''+@p_DatabaseName +''' as "Database name",  '+
-	''''+@p_SchemaName +''' as "Schema name", '+ 
-	''''+[Table_Name] + ''' as "Table name",'+
-	'count (*)  as "Total row count", '+
-	''''+[Column_name] + ''' as "Column name",'+
-	'i.DATA_TYPE  as "Data type",'
-	+'count(distinct ' + [Column_name] +') as "Count of DISTINCT values",'+
+					THEN 'SELECT   '+ 
+	''''+@p_DatabaseName +''' AS "Database name",  '+ -- select DB name
+	''''+@p_SchemaName +''' AS "Schema name", '+  --Shema name
+	''''+[Table_Name] + ''' AS "Table name",'+ --Table name
+	'COUNT (*)  AS "Total row count", '+ -- count of rows
+	''''+[Column_name] + ''' AS "Column name",'+ --column name
+	'i.DATA_TYPE  AS "Data type",' -- data type of colunm
+	+'count(distinct ' + [Column_name] +') AS "Count of DISTINCT values",'+ -- number of distinct values
+	't1."Count of NULL values", '+  --count of null values in column
+	't2."Count of empty/zero values", '+ -- count of empty or zero values in table
+	't3."Only UPPERCASE strings",'+ -- count of only uppercase strings
+	't4."Only LOWERCASE strings",'+ -- count of only lowercase strings
+	't5."Rows with non-printable characters at the beginning/end", '+ -- count of rows with begin or end of non printable values
+	'cast (t6."Most used value" as varchar) AS "Most used value",'+ -- most used value in column
+	' CAST(( select  t6."% rows with most used value") as numeric(6,2))  AS "% rows with most used value",'+ --persent of most used value in column
+	'cast (t7."MIN value"as varchar) AS "MIN value", cast (t7."MAX value"as varchar) AS "MAX Value"'+ -- min and max value in column
+	'FROM (select COUNT(*) AS "Count of NULL values" '+
+	'FROM  ['+@p_SchemaName+'].[' +[Table_Name]+ '] '+
+	' WHERE '+ [Column_name]+' IS NULL) AS t1 ' -- subquery for calculating count of null values
+	+'JOIN (SELECT SUM(CASE 
+             WHEN ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 1 AND cast ('+[Column_name]+'  as varchar ) =  ''0'' THEN 1 
+             WHEN ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0 AND cast ('+[Column_name]+'  as varchar) =  '''' THEN 1 
+			 ELSE 0
+           END) AS "Count of empty/zero values" 
+FROM  ['+@p_SchemaName+'].[' +[Table_Name]+ '])  AS t2 ON 1=1 '+ -- subquery for calculating count of empty or zero values
+'JOIN (SELECT SUM(CASE 
+			WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0) THEN 0
+             WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0 AND Upper('+[Column_name]+') COLLATE Latin1_General_CS_AS = '+[Column_name]+' ) THEN 1 
+			 ELSE 0
+			 END)
+           AS "Only UPPERCASE strings"  
+FROM  ['+@p_SchemaName+'].[' +[Table_Name]+ ']) AS t3 ON 1 = 1 '+ -- subquery for calculating only uppercase strings
+'JOIN (SELECT SUM(CASE 
+			WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0) THEN 0
+             WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0 AND Lower('+[Column_name]+') COLLATE Latin1_General_CS_AS = '+[Column_name]+' ) THEN 1 
+			 ELSE 0
+			 END)
+           AS "Only LOWERCASE strings"  
+FROM  ['+@p_SchemaName+'].[' +[Table_Name]+ ']) AS t4 ON 1 = 1 '  -- subquery for calculating only lowercase strings
++'JOIN (
+SELECT SUM (
+CASE WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 1) THEN 0
+WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0) AND 
+(PATINDEX(''['+ CHAR(10)+ Char(13)+ Char(9)  + ' ]%'', ((cast ( '+[Column_name]+' as varchar) ) ))=1 
+OR PATINDEX(''['+ CHAR(10)+ Char(13)+ Char(9)  + ' ]%'', REVERSE(cast ( '+[Column_name]+' as varchar)))=1) THEN 1
+ELSE 0
+END) AS "Rows with non-printable characters at the beginning/end"
+FROM ['+@p_SchemaName+'].[' +[Table_Name]+ ']) AS t5 ON 1=1 '  -- subquery for calculating Rows with non-printable characters at the beginning/end
++' JOIN (
+SELECT '+[Column_name] +' AS "Most used value", COUNT(*) AS count , 
+       COUNT(*) * 100.0 / SUM(COUNT(*)) OVER() AS "% rows with most used value",
+       SUM(CASE WHEN ' +[Column_name] + '  = popular_value THEN 1 ELSE 0 END) AS popular_count
+FROM ['+@p_SchemaName+'].[' +[Table_Name]+ ']
+CROSS APPLY (SELECT TOP 1 ' +[Column_name] + ' AS popular_value
+             FROM ['+@p_SchemaName+'].[' +[Table_Name]+ ']
+             GROUP BY ' +[Column_name] + '
+             ORDER BY COUNT(*) DESC) AS most_popular
+GROUP BY  ' +[Column_name] + '
+)  AS t6  -- subquery for calculating most popular value and persent of it value
+ON t6.popular_count !=0 '
++'JOIN  (
+SELECT MIN('+[Column_name]+') AS "MIN value", MAX('+[Column_name]+') AS "MAX value"
+FROM ['+@p_SchemaName+'].[' +[Table_Name]+ '])  AS t7 ON 1 = 1' -- subquery for calculating max and min value
+		+'JOIN ['+@p_SchemaName+'].[' +[Table_Name]+ ']  t ON 1=1 '+
+	'JOIN INFORMATION_SCHEMA.COLUMNS i ON i.TABLE_SCHEMA = '''+ @p_SchemaName+ 
+	''' AND i.TABLE_NAME = ''' +[Table_Name]+''''+
+	' AND i.COLUMN_NAME = '''+ [Column_name]+ ''''+
+	'  GROUP BY  i.data_type,   t1."Count of NULL values", t2."Count of empty/zero values",t3."Only UPPERCASE strings", t4."Only LOWERCASE strings" ,
+	t5."Rows with non-printable characters at the beginning/end", t6."Most used value", t6."% rows with most used value",t7."MIN value", t7."MAX value"  UNION ALL '
+				ELSE  -- if last row do not use union all in string
+					'SELECT   '+ 
+	''''+@p_DatabaseName +''' AS "Database name",  '+
+	''''+@p_SchemaName +''' AS "Schema name", '+ 
+	''''+[Table_Name] + ''' AS "Table name",'+
+	'COUNT (*)  AS "Total row count", '+
+	''''+[Column_name] + ''' AS "Column name",'+
+	'i.DATA_TYPE  AS "Data type",'
+	+'count(distinct ' + [Column_name] +') AS "Count of DISTINCT values",'+
 	't1."Count of NULL values", '+
 	't2."Count of empty/zero values", '+
 	't3."Only UPPERCASE strings",'+
-	't4."Only LOWERCASE strings"'+
-	'from (select COUNT(*) as "Count of NULL values" '+
-	'from  ['+@p_SchemaName+'].[' +[Table_Name]+ '] '+
-	' where '+ [Column_name]+' is NULL) as t1 '
-	+'join (SELECT COUNT(CASE 
-             WHEN ISNUMERIC('+[Column_name]+') = 1 AND '+[Column_name]+' = 0 THEN 1 
-             WHEN ISNUMERIC('+[Column_name]+') = 0 AND '+[Column_name]+' = '''' THEN 1 
+	't4."Only LOWERCASE strings",'+
+	't5."Rows with non-printable characters at the beginning/end", '+
+	'cast (t6."Most used value" as varchar) AS "Most used value",'+
+	' CAST(( select  t6."% rows with most used value") as numeric(6,2))  AS "% rows with most used value",'+
+	'cast (t7."MIN value"as varchar) AS "MIN value", cast (t7."MAX value"as varchar) AS "MAX Value"'+
+	'FROM (select COUNT(*) AS "Count of NULL values" '+
+	'FROM  ['+@p_SchemaName+'].[' +[Table_Name]+ '] '+
+	' WHERE '+ [Column_name]+' IS NULL) AS t1 '
+	+'JOIN (SELECT SUM(CASE 
+             WHEN ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 1 AND cast ('+[Column_name]+'  as varchar ) =  ''0'' THEN 1 
+             WHEN ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0 AND cast ('+[Column_name]+'  as varchar) =  '''' THEN 1 
+			 ELSE 0
            END) AS "Count of empty/zero values" 
-from  ['+@p_SchemaName+'].[' +[Table_Name]+ '])  as t2 on 1=1 '+
-'join (SELECT sum(CASE 
-			WHEN (ISNUMERIC('+[Column_name]+') = 0) THEN 0
-             WHEN (ISNUMERIC('+[Column_name]+') = 0 AND Upper('+[Column_name]+') COLLATE Latin1_General_CS_AS = '+[Column_name]+' ) THEN 1 
-			 else 0
-			 end)
+FROM  ['+@p_SchemaName+'].[' +[Table_Name]+ '])  AS t2 ON 1=1 '+
+'JOIN (SELECT SUM(CASE 
+			WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0) THEN 0
+             WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0 AND Upper('+[Column_name]+') COLLATE Latin1_General_CS_AS = '+[Column_name]+' ) THEN 1 
+			 ELSE 0
+			 END)
            AS "Only UPPERCASE strings"  
-from  ['+@p_SchemaName+'].[' +[Table_Name]+ ']) as t3 on 1 = 1 '+
-'join (SELECT sum(CASE 
-			WHEN (ISNUMERIC('+[Column_name]+') = 0) THEN 0
-             WHEN (ISNUMERIC('+[Column_name]+') = 0 AND Lower('+[Column_name]+') COLLATE Latin1_General_CS_AS = '+[Column_name]+' ) THEN 1 
-			 else 0
-			 end)
+FROM  ['+@p_SchemaName+'].[' +[Table_Name]+ ']) AS t3 ON 1 = 1 '+
+'JOIN (SELECT SUM(CASE 
+			WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0) THEN 0
+             WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0 AND Lower('+[Column_name]+') COLLATE Latin1_General_CS_AS = '+[Column_name]+' ) THEN 1 
+			 ELSE 0
+			 END)
            AS "Only LOWERCASE strings"  
-from  ['+@p_SchemaName+'].[' +[Table_Name]+ ']) as t4 on 1 = 1 '
-		+'join ['+@p_SchemaName+'].[' +[Table_Name]+ ']  t on 1=1 '+
-	'join INFORMATION_SCHEMA.COLUMNS i on i.TABLE_SCHEMA = '''+ @p_SchemaName+ 
-	''' and i.TABLE_NAME = ''' +[Table_Name]+''''+
-	' and i.COLUMN_NAME = '''+ [Column_name]+ ''''+
-	'  group by  i.data_type,   t1."Count of NULL values", t2."Count of empty/zero values",t3."Only UPPERCASE strings", t4."Only LOWERCASE strings"  UNION ALL '
-				ELSE 
-					'SELECT COUNT(*) [rows_cnt], ''' + [Table_Name] + ''' [Table_Name] '' FROM [hr].[' + [Table_Name] + '] '
+FROM  ['+@p_SchemaName+'].[' +[Table_Name]+ ']) AS t4 ON 1 = 1 '
++'JOIN (
+SELECT SUM (
+CASE WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 1) THEN 0
+WHEN (ISNUMERIC(cast ('+[Column_name]+' as varchar)) = 0) AND 
+(PATINDEX(''['+ CHAR(10)+ Char(13)+ Char(9)  + ' ]%'', ((cast ( '+[Column_name]+' as varchar) ) ))=1 
+OR PATINDEX(''['+ CHAR(10)+ Char(13)+ Char(9)  + ' ]%'', REVERSE(cast ( '+[Column_name]+' as varchar)))=1) THEN 1
+ELSE 0
+END) AS "Rows with non-printable characters at the beginning/end"
+FROM ['+@p_SchemaName+'].[' +[Table_Name]+ ']) AS t5 ON 1=1 '
++' JOIN (
+SELECT '+[Column_name] +' AS "Most used value", COUNT(*) AS count , 
+       COUNT(*) * 100.0 / SUM(COUNT(*)) OVER() AS "% rows with most used value",
+       SUM(CASE WHEN ' +[Column_name] + '  = popular_value THEN 1 ELSE 0 END) AS popular_count
+FROM ['+@p_SchemaName+'].[' +[Table_Name]+ ']
+CROSS APPLY (SELECT TOP 1 ' +[Column_name] + ' AS popular_value
+             FROM ['+@p_SchemaName+'].[' +[Table_Name]+ ']
+             GROUP BY ' +[Column_name] + '
+             ORDER BY COUNT(*) DESC) AS most_popular
+GROUP BY  ' +[Column_name] + '
+)  AS t6 
+ON t6.popular_count !=0 '
++'JOIN  (
+SELECT MIN('+[Column_name]+') AS "MIN value", MAX('+[Column_name]+') AS "MAX value"
+FROM ['+@p_SchemaName+'].[' +[Table_Name]+ '])  AS t7 ON 1 = 1'
+		+'JOIN ['+@p_SchemaName+'].[' +[Table_Name]+ ']  t ON 1=1 '+
+	'JOIN INFORMATION_SCHEMA.COLUMNS i ON i.TABLE_SCHEMA = '''+ @p_SchemaName+ 
+	''' AND i.TABLE_NAME = ''' +[Table_Name]+''''+
+	' AND i.COLUMN_NAME = '''+ [Column_name]+ ''''+
+	'  GROUP BY  i.data_type,   t1."Count of NULL values", t2."Count of empty/zero values",t3."Only UPPERCASE strings", t4."Only LOWERCASE strings" ,
+	t5."Rows with non-printable characters at the beginning/end", t6."Most used value", t6."% rows with most used value",t7."MIN value", t7."MAX value"  '
 			END [query_text]
 		FROM tbl_list
+)
 
 
+SELECT -- form the query 
+	@v_Query = STRING_AGG([query_text], '') WITHIN GROUP (ORDER BY [query_text])
+FROM query_not_agg;
 
-SELECT * FROM hr.countries WHERE [country_name] REGEXP '[[:space:]]'
-SELECT * FROM hr.countries
-WHERE country_name LIKE '^\s%' -- Check if there are any non-printable characters at the beginning of the string
-OR country_name LIKE '^\s'
-Select   'TRN' as "Database name",  'hr' as "Schema name", 'countries' as "Table name",count (*)  as "Total row count", 'country_id' as "Column name",i.DATA_TYPE  as "Data type",count(distinct country_id) as "Count of DISTINCT values",t1."Count of NULL values", t2."Count of empty/zero values", t3."Only UPPERCASE strings",t4."Only LOWERCASE strings"from (select COUNT(*) as "Count of NULL values" from  [hr].[countries]  where country_id is NULL) as t1 join (SELECT COUNT(CASE                WHEN ISNUMERIC(country_id) = 1 AND country_id = 0 THEN 1                WHEN ISNUMERIC(country_id) = 0 AND country_id = '' THEN 1              END) AS "Count of empty/zero values"   from  [hr].[countries])  as t2 on 1=1 join (SELECT sum(CASE      WHEN (ISNUMERIC(country_id) = 0) THEN 0               WHEN (ISNUMERIC(country_id) = 0 AND Upper(country_id) COLLATE Latin1_General_CS_AS = country_id ) THEN 1       else 0      end)             AS "Only UPPERCASE strings"    from  [hr].[countries]) as t3 on 1 = 1 join (SELECT sum(CASE      WHEN (ISNUMERIC(country_id) = 0) THEN 0               WHEN (ISNUMERIC(country_id) = 0 AND Lower(country_id) COLLATE Latin1_General_CS_AS = country_id ) THEN 1       else 0      end)             AS "Only LOWERCASE strings"    from  [hr].[countries]) as t4 on 1 = 1 join [hr].[countries]  t on 1=1 join INFORMATION_SCHEMA.COLUMNS i on i.TABLE_SCHEMA = 'hr' and i.TABLE_NAME = 'countries' and i.COLUMN_NAME = 'country_id'  group by  i.data_type,   t1."Count of NULL values", t2."Count of empty/zero values",t3."Only UPPERCASE strings", t4."Only LOWERCASE strings" 
---Select   'TRN' as "Database name",  'hr' as "Schema name", 'countries' as "Table name",count (*)  as "Total row count", 'country_id' as "Column name",i.DATA_TYPE  as "Data type",count(distinct country_id) as "Count of DISTINCT values",t1."Count of NULL values", t2."Count of empty/zero values", t3."Only UPPERCASE strings",t4."Only LOWERCASE strings"from (select COUNT(*) as "Count of NULL values" from  [hr].[countries]  where country_id is NULL) as t1 join (SELECT COUNT(CASE                WHEN ISNUMERIC(country_id) = 1 AND country_id = 0 THEN 1                WHEN ISNUMERIC(country_id) = 0 AND country_id = '' THEN 1              END) AS "Count of empty/zero values"   from  [hr].[countries])  as t2 on 1=1 join (SELECT sum(CASE      WHEN (ISNUMERIC(country_id) = 0) THEN 0               WHEN (ISNUMERIC(country_id) = 0 AND Upper(country_id) COLLATE Latin1_General_CS_AS = country_id ) THEN 1       else 0      end)             AS "Only UPPERCASE strings"    from  [hr].[countries]) as t3 on 1 = 1 join (SELECT sum(CASE      WHEN (ISNUMERIC(country_id) = 0) THEN 0               WHEN (ISNUMERIC(country_id) = 0 AND Lower(country_id) COLLATE Latin1_General_CS_AS = country_id ) THEN 1       else 0      end)             AS "Only LOWERCASE strings"    from  [hr].[countries]) as t4 on 1 = 1 join [hr].[countries]  t on 1=1 join INFORMATION_SCHEMA.COLUMNS i on i.TABLE_SCHEMA = 'hr' and i.TABLE_NAME = 'countries' and i.COLUMN_NAME = 'country_id'  group by  i.data_type,   t1."Count of NULL values", t2."Count of empty/zero values",t3."Only UPPERCASE strings"  
---Select   'TRN' as "Database name",  'hr' as "Schema name", 'countries' as "Table name",count (*)  as "Total row count", 'country_id' as "Column name",i.DATA_TYPE  as "Data type",count(distinct country_id) as "Count of DISTINCT values",t1."Count of NULL values", t2."Count of empty/zero values", t3."Only UPPERCASE strings"from (select COUNT(*) as "Count of NULL values" from  [hr].[countries]  where country_id is NULL) as t1 join (SELECT COUNT(CASE                WHEN ISNUMERIC(country_id) = 1 AND country_id = 0 THEN 1                WHEN ISNUMERIC(country_id) = 0 AND country_id = '' THEN 1              END) AS "Count of empty/zero values"   from  [hr].[countries])  as t2 on 1=1 join (SELECT sum(CASE      WHEN (ISNUMERIC(country_id) = 0) THEN 0               WHEN (ISNUMERIC(country_id) = 0 AND Upper(country_id) COLLATE Latin1_General_CS_AS = country_id ) THEN 1       else 0      end)             AS "Only UPPERCASE strings"    from  [hr].[countries]) as t3 on 1 = 1 join [hr].[countries]  t on 1=1 join INFORMATION_SCHEMA.COLUMNS i on i.TABLE_SCHEMA = 'hr' and i.TABLE_NAME = 'countries' and i.COLUMN_NAME = 'country_id'  group by  i.data_type,   t1."Count of NULL values", t2."Count of empty/zero values",t3."Only UPPERCASE strings"  
-/*DECLARE @v_Query NVARCHAR(MAX);
-SET @v_Query = 
-'Select  '' '+ 
-	@p_DatabaseName +' '' as "Database name", '' '+
-	@p_SchemaName +' '' as "Schema name", '' '+
-	@p_TableName +' ''  as "Table name", '+
-	'COUNT(*)  FROM ['+@p_SchemaName+'].[' + @p_TableName + '] as "Total row count"'
-;
---select  @v_Query;*/
 
---EXEC SP_EXECUTESQL @v_Query;*/
-
+EXEC SP_EXECUTESQL @v_Query; --execute query
 Return;
 GO  
-
 
 DECLARE @DatabaseName NVARCHAR(max), @SchemaName NVARCHAR(max), @TableName NVARCHAR(max);
 SET @DatabaseName = 'TRN'
 SET @SchemaName = 'hr'
-SET @TableName = '%'--'countries'--'regions'
+SET @TableName = '%'
 
  
-EXEC Statistics_pr @DatabaseName, @SchemaName, @TableName
- 
+EXEC Statistics_pr @DatabaseName, @SchemaName, @TableName -- execute the procedure
